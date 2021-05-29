23bbb89e1e0217d1c42563d8d20509d8df48f377
Patrick Magalhães <patrickcmagal@gmail.com>
2021-05-29 16:15:50 -0300

    fix

--- lib/asaas/models/subscription.rb
+++ lib/asaas/models/subscription.rb
@@ -1,5 +1,13 @@
 module Asaas
-  class subscription < Model
+  class Subscription < Model
     attribute :id, Types::Coercible::String.optional.default(nil)
+    attribute :value, Types::Coercible::Decimal.optional.default(nil)
+    attribute :description, Types::Coercible::String.optional.default(nil)
+    attribute :endDate, Types::Coercible::String.optional.default(nil)
+    attribute :cycle, Types::Coercible::String.optional.default(nil)
+    attribute :nextDueDate, Types::Coercible::String.optional.default(nil)
+
+
+
   end
 end
